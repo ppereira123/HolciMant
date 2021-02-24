d1c130664ffac19d234eb94ef82aea1f6e501b77
ppereira123 <52977467+ppereira123@users.noreply.github.com>
2021-02-24 11:34:02 -0500

    spinner estantes

--- MantenimientoHolcim/app/src/main/java/com/example/mantenimientoholcim/CrearItem.java
+++ MantenimientoHolcim/app/src/main/java/com/example/mantenimientoholcim/CrearItem.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 public class CrearItem extends AppCompatActivity {
     EditText descripcionTxt,marcaTxt,observacionTxt,codigoTxt;
-    Spinner estadoActualSpinner,ubicacionSpinner,tipoInspeccionspinner;
+    Spinner estadoActualSpinner,ubicacionSpinner,tipoInspeccionspinner, estanteSpinner;
     NumberPicker vidaUtilTxt;
     private String estado="",ubicacion="",tipoInspeccion="";
     boolean generarCodigo=false;
@@ -32,6 +32,7 @@ public class CrearItem extends AppCompatActivity {
     Item item=null;
     Button btnGenerar;
     int stock=1;
+    ArrayAdapter<String> adapterestanteSpinner;
 
     public CrearItem(){
 
@@ -49,6 +50,7 @@ public class CrearItem extends AppCompatActivity {
         estadoActualSpinner=findViewById(R.id.estadoActualSpinner);
         ubicacionSpinner=findViewById(R.id.spinnerUbicacion);
         tipoInspeccionspinner=findViewById(R.id.spinnerInspeccion);
+        estanteSpinner=findViewById(R.id.spinnerEstante);
         codigoTxt=findViewById(R.id.codigotxt);
         vidaUtilTxt.setMinValue(0);
         vidaUtilTxt.setMaxValue(50);
@@ -61,12 +63,17 @@ public class CrearItem extends AppCompatActivity {
 
 
     void cargarSpinners(){
-        List<String> estados= new ArrayList<>();
-        List<String> ubicaciones= new ArrayList<>();
-        List<String> tipos= new ArrayList<>();
+        ArrayList<String> estados= new ArrayList<>();
+        ArrayList<String> ubicaciones= new ArrayList<>();
+        ArrayList<String> tipos= new ArrayList<>();
+
         estados.add("");
-        ubicaciones.add(0,"Bodega de Materiales");
-        ubicaciones.add(1,"Bodega de Herramientas");
+        ubicaciones.add(0,"Bodega de Herramientas");
+        ubicaciones.add(1,"Bodega de Materiales");
+        ubicaciones.add(2,"Bodega Lubricantes");
+        ubicaciones.add(3,"Bodega.");
+        ubicaciones.add(4,"Bodega de llantas y componentes grandes");
+
         tipos.add(0,"N/A");
         tipos.add(1,"Trimestral");
         tipos.add(2,"Quinquenal");
@@ -81,6 +88,71 @@ public class CrearItem extends AppCompatActivity {
         estadoActualSpinner.setAdapter(adapterEstados);
         ubicacionSpinner.setAdapter(adapterUbicaciones);
         tipoInspeccionspinner.setAdapter(adapterTipos);
+
+        //Spinner dependiente//
+        ArrayList<String> estante= new ArrayList<>();
+        ArrayList<String> bodegaMateriales= new ArrayList<>();
+        bodegaMateriales.add("Aceite");
+        bodegaMateriales.add("Grasas");
+        bodegaMateriales.add("Refigerante");
+        ArrayList<String> bodegaHerramientas= new ArrayList<>();
+        bodegaHerramientas.add("1A");
+        bodegaHerramientas.add("1B");
+        bodegaHerramientas.add("1C");
+        bodegaHerramientas.add("1D");
+        bodegaHerramientas.add("1E");
+        bodegaHerramientas.add("2A");
+        bodegaHerramientas.add("2B");
+        bodegaHerramientas.add("2C");
+        bodegaHerramientas.add("2D");
+        bodegaHerramientas.add("3A");
+        bodegaHerramientas.add("3B");
+        bodegaHerramientas.add("3C");
+        bodegaHerramientas.add("3D");
+        bodegaHerramientas.add("1P");
+        bodegaHerramientas.add("2P");
+        bodegaHerramientas.add("3P");
+        bodegaHerramientas.add("4P");
+        ArrayList<String> bodegaLubricantes= new ArrayList<>();
+        bodegaLubricantes.add("N/A");
+        ArrayList<String> bodega= new ArrayList<>();
+        bodegaLubricantes.add("N/A");
+        ArrayList<String> bodegaLlantas= new ArrayList<>();
+        bodegaLlantas.add("N/A");
+
+        ubicacionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    adapterestanteSpinner=new ArrayAdapter<>(CrearItem.this, android.R.layout.simple_dropdown_item_1line,bodegaHerramientas);
+                }
+                else if(position==1){
+                    adapterestanteSpinner= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, bodegaMateriales);
+                }
+                else if(position==2){
+                    adapterestanteSpinner= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, bodegaLubricantes);
+                }
+                else if(position==3){
+                    adapterestanteSpinner= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, bodega);
+                }
+                else if(position==4){
+                    adapterestanteSpinner= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_item, bodegaLlantas);
+                }
+                else
+                    adapterestanteSpinner=new ArrayAdapter<>(CrearItem.this, android.R.layout.simple_dropdown_item_1line,bodegaHerramientas);
+                estanteSpinner.setAdapter(adapterestanteSpinner);
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        //Spinner dependiente fin//
         spinnerEstado();
         spinnerTipo();
         spinnerUbicaciones();
